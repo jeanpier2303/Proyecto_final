70d9af72a100875a99ed0604977f7165ca83c4be
DaironOrejuela <stiven3222177@gmail.com>
2025-10-23 12:31:25 -0500

    GOOD

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,17 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import NavbarApp from "./components/NavbarApp";
 import Login from "./pages/Login/Login";
-import RegisterFORM from "./pages/Registro/Register";
+import Register from "./pages/Register/Register"; 
 
 function App() {
+
   return (
     <Router>
-      <NavbarApp />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<RegisterFORM />} />
+        <Route path="/register" element={<Register />} /> 
       </Routes>
     </Router>
   );
